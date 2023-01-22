70dd6a8fac8f55361949dd5d193c57f19cf8ddef
Ty Mason <tygerxqt@proton.me>
2023-01-22 15:53:49 +0000

    Add redirects

--- next.config.js
+++ next.config.js
@@ -2,6 +2,30 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/X8pEdnEmuY',
+        permanent: true,
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/nord-studio',
+        permanent: true,
+      },
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/wearenordstudio',
+        permanent: true,
+      },
+      {
+        source: '/instagram',
+        destination: 'https://instagram.com/wearenordstudio',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
